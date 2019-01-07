731aadea9c6e2d8871bf68b76d69ee24279c0d8d
JulvDen <julien-denis@hotmail.com>
2019-01-07 15:01:26 +0100

    Updated admin to support custom user model.

--- app/core/admin.py
+++ app/core/admin.py
@@ -10,8 +10,10 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields':('name',)}),
-        (_('Permissions'),{'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+        _('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
         (_('Important dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
@@ -20,5 +22,6 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
+    
 
 admin.site.register(models.User, UserAdmin)
